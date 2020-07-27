13c0db2812fef30ffa0270f3895070c3e304f97c
Benzo26 <bugesi.benzo@gmail.com>
2020-07-27 07:57:59 +0300

    add function to alert receipt message to user

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,23 @@ $(document).ready(function(){
     $("#products,#hide-products").click(function(){
     $("#hide-products").toggle();
     });
-    
+
     $('.image-frame').hover(function(){
     $('.image-caption',this).slideToggle('slow');
     }, function(){
         $('.image-caption',this).slideToggle('slow');
+    });
+
+    $("form").submit(function(){
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('textarea').value;
+        if (name !=="" && email !=="" && message !==""){
+          alert("Thank you" + " "+ name + " "+ "we have received your message. Thank you for reaching out to us. ");
+        }else{
+          alert("Insert your name, email and message");
+        }
+        event.preventDefault();
+        
     });
 })
